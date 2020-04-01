8e045672d986e14e431a83509bc98af1dbe9e6a8
r.nishino <r.nishino@stonesystem.co.jp>
2020-04-01 15:11:57 +0900

    [add] レイアウト作成

---
+++ src/views/Calculator.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <input type="text" value="0" />
+    <table class="keypadTable">
+      <tr>
+        <td><button>７</button></td>
+        <td><button>８</button></td>
+        <td><button>９</button></td>
+        <td><button>➗</button></td>
+      </tr>
+      <tr>
+        <td><button>４</button></td>
+        <td><button>５</button></td>
+        <td><button>６</button></td>
+        <td><button>✖️</button></td>
+      </tr>
+      <tr>
+        <td><button>１</button></td>
+        <td><button>２</button></td>
+        <td><button>３</button></td>
+        <td><button>➖</button></td>
+      </tr>
+      <tr>
+        <td><button>０</button></td>
+        <td><button>．</button></td>
+        <td><button>＝</button></td>
+        <td><button>➕</button></td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style>
+.keypadTable {
+  margin-right: auto;
+  margin-left: auto;
+}
+</style>
